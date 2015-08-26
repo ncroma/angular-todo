e896ee030d87188cb3f1018eb213908285522ab0
Nuno Roma <nuno.roma@pacsis.pt>
2015-08-26 15:13:57 +0100

    novo view model e reflexo na estrutura de dados

--- js/app.js
+++ js/app.js
@@ -1,181 +1,119 @@
-function StorageService(){
+function StorageService() {
 
-	this.setStorage = function SetStorage(key, content){
-
-		/*            content.forEach(function(obj){delete obj.$$hashKey});
+    this.setStorage = function SetStorage(key, content) {
+        /*            content.forEach(function(obj){delete obj.$$hashKey});
 			-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	
 		angular.toJson retira a informação desnecessária que o angular usa para obter JSON limpo*/
+        window.localStorage.setItem(key, angular.toJson(content));
+    };
 
-		window.localStorage.setItem(key, angular.toJson(content));
-	};
-
-	this.getStorage = function GetStorage(key){
-		if (!window.localStorage.getItem(key)){
-			return [];
-		}
-		return JSON.parse(window.localStorage.getItem(key));		
-	};
+    this.getStorage = function GetStorage(key) {
+        if (!window.localStorage.getItem(key)) {
+            return [];
+        }
+        return JSON.parse(window.localStorage.getItem(key));
+    };
 }
 
 angular.module('app', [])
-	.factory('fabrica', function Fabrica(){
-		return {prop1:"fabrica",prop2:20};
-	})
-	.service('storageService', [StorageService])
-    .controller('AppCtrl', ['$scope','fabrica','storageService',
+    .service('storageService', [StorageService])
+    .controller('AppCtrl', ['$scope', 'storageService',
 
-        function($scope, fabrica, storageService) {
+        function($scope, storageService) {
 
             var self = this;
 
-            this.todos = storageService.getStorage('todos');
+            this.todosPorOrganizar = storageService.getStorage('todos');
+
             this.filterState = {};
             this.checkTodos = false;
 
-            this.persist = function(key, content){
-            	storageService.setStorage(key, content);
-            };
+            this.todosOrganizer = function todosOrganizer(todos) {
+                var todosOrganizados = [];
+                var todoArray = [];
+
+                if (todos.length) {
+                    for (var i = 0; i < todos.length; i++) {
+                        todoArray.push(todos[i]);
+                        if (!todos[i + 1]) {
+                            todosOrganizados.push(new todoPorDia(todos[i].date, todoArray));
+                        } else {
+                            if (howManyDaysBetween(todos[i].date, todos[i + 1].date) > 0) {
+                                todosOrganizados.push(new todoPorDia(todos[i].date, todoArray));
+                                todoArray = [];
+                            }
+                        }
+                    };
+                }
+                return todosOrganizados;
+            }
+
+            this.todosPorDia = self.todosOrganizer(self.todosPorOrganizar);
+
+            $scope.$watch('AppCtrl.todosPorOrganizar', function() {
+                self.todosPorDia = self.todosOrganizer(self.todosPorOrganizar);
+                storageService.setStorage('todos', self.todosPorOrganizar);
+            }, true);
+
 
             this.submit = function() {
                 if (self.todo) {
                     self.todo.completed = false;
-                    self.todos.push(self.todo);
+                    self.todo.date = Date.now();
+                    self.todosPorOrganizar.push(self.todo);
                     self.todo = null;
-           
-                    self.persist('todos', self.todos);
                 }
-
-            };
+            }
 
             this.changeTodos = function() {
-
-                for (var i = self.todos.length - 1; i >= 0; i--) {
-                    self.todos[i].completed = !(self.checkTodos);
-                };
-                self.persist('todos', self.todos);
-            };
-
-            /*
-			this.todosChecked = function() {
-				if (self.todos.length) {
-					for (var i = self.todos.length - 1; i >= 0; i--) {
-						if (!self.todos[i].completed) {
-							self.checkTodos = false;
-							return false;
-						}
-					};
-				}
-				self.checkTodos = true;
-				return true;
-			};
-			*/
+                for (var i = self.todosPorOrganizar.length - 1; i >= 0; i--) {
+                    self.todosPorOrganizar[i].completed = !(self.checkTodos);
+                }
+            }
 
             this.todosChecked = function() {
-                return self.checkTodos = self.todos.every(function(todo) {
+                return self.checkTodos = self.todosPorOrganizar.every(function(todo) {
                     return todo.completed;
                 });
-            };
+            }
 
             this.remove = function(todo) {
-                self.todos.splice(self.todos.indexOf(todo), 1);
-                self.persist('todos', self.todos);
-            };
+                self.todosPorOrganizar.splice(self.todosPorOrganizar.indexOf(todo), 1);
+            }
 
-
-            /*this.howManyChecked = function() {
-
-				return self.todos.map(function(todo) {
-					return todo.completed ? 1 : 0;
-				}).reduce(function(a, b) {
-					return a + b;
-				}, 0);
-			}*/
 
             this.howManyChecked = function() {
-
-                return self.todos.reduce(function(a, b) {
+                return self.todosPorOrganizar.reduce(function(a, b) {
                     return a + (b.completed ? 1 : 0);
                 }, 0);
             }
 
             this.howManyUnchecked = function() {
-
-                return self.todos.reduce(function(a, b) {
+                return self.todosPorOrganizar.reduce(function(a, b) {
                     b = !b.completed ? 1 : 0;
                     return a + b;
                 }, 0);
             }
 
             this.clearCompleted = function() {
-                for (var i = self.todos.length - 1; i >= 0; i--) {
-                    if (self.todos[i].completed) {
-                        self.todos.splice(i, 1);
+                for (var i = self.todosPorOrganizar.length - 1; i >= 0; i--) {
+                    if (self.todosPorOrganizar[i].completed) {
+                        self.todosPorOrganizar.splice(i, 1);
                     }
                 };
-               self.persist('todos', self.todos);
             }
 
+            function todoPorDia(dia, todos) {
+                this.dia = dia;
+                this.listaTodos = todos;
+            }
 
-            /*
-			this.howMany = function(comportamento, todos) {
-				var howMany = 0;
-				for (var i = todos.length - 1; i >= 0; i--) {
-					if (comportamento(todos[i])) {
-						howMany++
-					}
-				};
-				return howMany;
-			};
-
-			this.howManyUnchecked = function() {
-				return self.howMany(function teste(todo) {
-					return !todo.completed;
-				}, self.todos);
-			};
-
-			this.howManyChecked = function() {
-				return self.howMany(function teste(todo) {
-					return todo.completed;
-				}, self.todos);
-			};
-
-
-			this.howManyUnchecked2 = function() {
-				return self.todos.filter(function teste(todo) {
-					return !todo.completed;
-				}).length;
-			}
-
-			this.howManyChecked2 = function() {
-				return self.todos.filter(function teste(todo) {
-					return todo.completed;
-				}).length;
-			};
-
-			this.numArray = function(comportamento, todos) {
-				return todos.map(function(todo) {
-					if (comportamento(todo)) {
-						return 1;
-					}
-					return 0;
-				});
-			}
-
-			this.howManyChecked3 = function() {
-				return self.numArray(function(todo) {
-					return todo.completed
-				}, self.todos).reduce(function(a, b) {
-					return a + b;
-				});
-			}
-
-			this.howManyChecked3 = function() {
-				return self.numArray(function(todo) {
-					return !todo.completed
-				}, self.todos).reduce(function(a, b) {
-					return a + b;
-				});
-			}
-			*/
+            function howManyDaysBetween(past, future) {
+                var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+                var firstDate = future;
+                var secondDate = past;
+                var difference_ms = Math.abs(future - past);
+                return Math.round(difference_ms / oneDay);
+            }
         }
     ])
