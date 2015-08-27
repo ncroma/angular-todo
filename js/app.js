7f79c949ccbd500a1c4ad91bb7ca4b7e01b8c8cd
Nuno Roma <nuno.roma@pacsis.pt>
2015-08-27 18:02:55 +0100

    fixes

--- js/app.js
+++ js/app.js
@@ -25,33 +25,15 @@ angular.module('app', [])
 
             this.todosPorOrganizar = storageService.getStorage('todos');
 
-            this.filterState = {};
+            this.filterStateDia = {};
+            this.filterStateTodo = {};
             this.checkTodos = false;
 
-            this.todosOrganizer = function todosOrganizer(todos) {
-                var todosOrganizados = [];
-                var todoArray = [];
+            this.todosPorDia = todosOrganizer(self.todosPorOrganizar);
 
-                if (todos.length) {
-                    for (var i = 0; i < todos.length; i++) {
-                        todoArray.push(todos[i]);
-                        if (!todos[i + 1]) {
-                            todosOrganizados.push(new todoPorDia(todos[i].date, todoArray));
-                        } else {
-                            if (howManyDaysBetween(todos[i].date, todos[i + 1].date) > 0) {
-                                todosOrganizados.push(new todoPorDia(todos[i].date, todoArray));
-                                todoArray = [];
-                            }
-                        }
-                    };
-                }
-                return todosOrganizados;
-            }
-
-            this.todosPorDia = self.todosOrganizer(self.todosPorOrganizar);
 
             $scope.$watch('AppCtrl.todosPorOrganizar', function() {
-                self.todosPorDia = self.todosOrganizer(self.todosPorOrganizar);
+                self.todosPorDia = todosOrganizer(self.todosPorOrganizar);
                 storageService.setStorage('todos', self.todosPorOrganizar);
             }, true);
 
@@ -65,11 +47,13 @@ angular.module('app', [])
                 }
             }
 
-            this.changeTodos = function() {
-                for (var i = self.todosPorOrganizar.length - 1; i >= 0; i--) {
-                    self.todosPorOrganizar[i].completed = !(self.checkTodos);
-                }
+
+            this.changeTodos = function(todos, check) {
+	                for (var i = todos.length - 1; i >= 0; i--) {
+	                    self.todosPorOrganizar[self.todosPorOrganizar.indexOf(todos[i])].completed = !check;
+	                }
             }
+
 
             this.todosChecked = function() {
                 return self.checkTodos = self.todosPorOrganizar.every(function(todo) {
@@ -104,8 +88,23 @@ angular.module('app', [])
             }
 
             function todoPorDia(dia, todos) {
+            	var my = this;
                 this.dia = dia;
                 this.listaTodos = todos;
+                this.todosUnchecked = uncheckedTodos();
+                this.todosChecked = checkedTodos();
+
+                function checkedTodos(){ 
+                	return my.listaTodos.every(function(todo) {
+                    	return todo.completed;
+                	});
+                }
+
+                function uncheckedTodos(){ 
+                	return my.listaTodos.every(function(todo) {
+                    	return !todo.completed;
+                	});
+                }
             }
 
             function howManyDaysBetween(past, future) {
@@ -115,5 +114,87 @@ angular.module('app', [])
                 var difference_ms = Math.abs(future - past);
                 return Math.round(difference_ms / oneDay);
             }
+
+            function todosOrganizer(todos) {
+                var todosOrganizados = [];
+                var todoArray = [];
+
+                if (todos.length) {
+                    for (var i = 0; i < todos.length; i++) {
+                        todoArray.push(todos[i]);
+                        if (!todos[i + 1]) {
+                            todosOrganizados.push(new todoPorDia(todos[i].date, todoArray));
+                        } else {
+                            if (howManyDaysBetween(todos[i].date, todos[i + 1].date) > 0) {
+                                todosOrganizados.push(new todoPorDia(todos[i].date, todoArray));
+                                todoArray = [];
+                            }
+                        }
+                    };
+                }
+                return todosOrganizados;
+            }
         }
     ])
+
+ /*
+-			this.howMany = function(comportamento, todos) {
+-				var howMany = 0;
+-				for (var i = todos.length - 1; i >= 0; i--) {
+-					if (comportamento(todos[i])) {
+-						howMany++
+-					}
+-				};
+-				return howMany;
+-			};
+-
+-			this.howManyUnchecked = function() {
+-				return self.howMany(function teste(todo) {
+-					return !todo.completed;
+-				}, self.todos);
+-			};
+-
+-			this.howManyChecked = function() {
+-				return self.howMany(function teste(todo) {
+-					return todo.completed;
+-				}, self.todos);
+-			};
+-
+-
+-			this.howManyUnchecked2 = function() {
+-				return self.todos.filter(function teste(todo) {
+-					return !todo.completed;
+-				}).length;
+-			}
+-
+-			this.howManyChecked2 = function() {
+-				return self.todos.filter(function teste(todo) {
+-					return todo.completed;
+-				}).length;
+-			};
+-
+-			this.numArray = function(comportamento, todos) {
+-				return todos.map(function(todo) {
+-					if (comportamento(todo)) {
+-						return 1;
+-					}
+-					return 0;
+-				});
+-			}
+-
+-			this.howManyChecked3 = function() {
+-				return self.numArray(function(todo) {
+-					return todo.completed
+-				}, self.todos).reduce(function(a, b) {
+-					return a + b;
+-				});
+-			}
+-
+-			this.howManyChecked3 = function() {
+-				return self.numArray(function(todo) {
+-					return !todo.completed
+-				}, self.todos).reduce(function(a, b) {
+-					return a + b;
+-				});
+-			}
+-			*/
